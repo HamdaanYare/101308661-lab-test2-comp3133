7754058093d2fabeb3e62f244749a4e63b17694f
HamdaanYare <fanahyare114@gmail.com>
2023-04-03 02:18:58 -0500

    app.component.ts updated

--- 101308661-lab-test2-comp3133/src/app/app.component.ts
+++ 101308661-lab-test2-comp3133/src/app/app.component.ts
@@ -1,28 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title = '101308661-lab-test2-comp3133';
-// }
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-missionlist',
-  templateUrl: './missionlist/missionlist.component.html', // missionlist.component.html
-  styleUrls: ['./missionlist/missionlist.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
-export class MissionlistComponent implements OnInit {
-  missions: any[] | undefined;
-
-  constructor(private http: HttpClient) { }
-
-  ngOnInit(): void {
-    this.http.get<any[]>('https://api.spacexdata.com/v3/launches').subscribe(data => {
-      this.missions = data;
-    });
-  }
+export class AppComponent {
+  title = '101308661-lab-test2-comp3133';
 }
