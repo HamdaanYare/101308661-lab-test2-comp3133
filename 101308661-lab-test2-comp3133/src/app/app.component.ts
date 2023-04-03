afd10b9ab8826b42372c529417cf0908c6117e72
HamdaanYare <fanahyare114@gmail.com>
2023-04-03 00:37:09 -0500

    added missionlist component

--- 101308661-lab-test2-comp3133/src/app/app.component.ts
+++ 101308661-lab-test2-comp3133/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = '101308661-lab-test2-comp3133';
+// }
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-missionlist',
+  templateUrl: './missionlist/missionlist.component.html', // missionlist.component.html
+  styleUrls: ['./missionlist/missionlist.component.css']
 })
-export class AppComponent {
-  title = '101308661-lab-test2-comp3133';
+export class MissionlistComponent implements OnInit {
+  missions: any[] | undefined;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<any[]>('https://api.spacexdata.com/v3/launches').subscribe(data => {
+      this.missions = data;
+    });
+  }
 }
--- 101308661-lab-test2-comp3133/src/app/app.module.ts
+++ 101308661-lab-test2-comp3133/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// import { AppComponent } from './app.component';
+import { AppComponent } from 'angular/src/app/app.component';
+import { MissionlistComponent } from './missionlist/missionlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MissionlistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
